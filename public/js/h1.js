1c9c435d67d93d3d9da42b817a293ea668271399
Luis Rodriguez <36767624+lenrodca@users.noreply.github.com>
2020-10-27 17:01:06 -0500

    Añadido segundo vehiculo

--- public/js/h1.js
+++ public/js/h1.js
@@ -1,97 +1,152 @@
-window.onload = function(){
-    var lat, lon, fecha, hora, mensaj, poli;
-    var marcador = L.marker([0, 0]);
-    var f1,f2,h1,h2,btn,def;
-    def = this.document.getElementById("dtp")
-    var road=[];
-    var roadtime=[];
-    btn = this.document.getElementById("button");
-    let map = L.map('map').setView([10.99304, -74.82814], 12);
-    const tileurl2 = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    L.tileLayer(tileurl2).addTo(map);
-    
-    marcador = L.marker([0, 0]);
-    marcador.addTo(map);
-    const date = new Date();
-    var mes = (date.getMonth()+1)*0.01;
-    def.value= date.getFullYear()+ "-" + mes.toString().slice(2,5) + "-" + date.getDate();
-    $(function() {
-        $('input[name="datetimes"]').daterangepicker({
-          timePicker: true,
-          timerFormat: 'HH:mm:ss',
-          singleDatePicker: false ,
-          startDate: moment().subtract(12, 'hour'),
-          endDate: moment(),
-          timePicker24Hour: false,
-          timePickerIncrement: 15,
-          timePickerSeconds: false,
-          "maxDate": moment(),
-          startValue: moment(),
-          locale: {
-            format: 'YYYY-MM-DD HH:mm:ss A'
-          },
-          
-        },function(start,end) {
-        });
-        $('#dtp').html(moment().subtract('days', 29).format('D MMMM YYYY') + ' gsdg' + moment().format('D MMMM YYYY'));
-       
-    });
-    
+window.onload = function () {
+	var lat, lon, fecha, hora, mensaj, poli, mensaje;
+	var marcador = L.marker([0, 0]);
+	var f1, f2, h1, h2, btn, def;
+	def = this.document.getElementById('dtp');
+	var road = [];
+	var roadtime = [];
+	btn = this.document.getElementById('button');
+	let map = L.map('map').setView([10.99304, -74.82814], 12);
+	const tileurl2 = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	L.tileLayer(tileurl2).addTo(map);
+	var greenIcon = new L.Icon({
+		iconUrl:
+			'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+		shadowUrl:
+			'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41],
+	});
 
-    btn.addEventListener("click",async function(){
-        f1 = def.value.slice(0,10);
-        f2 = def.value.slice(24,35);
-        h1 = def.value.slice(11,19);
-        h2 = def.value.slice(36,44);
-        // console.log(def.value)
-        data={
-            fecha1: f1, fecha2: f2, hora1: h1, hora2: h2
-        }
-        
-        // console.log("La consulta es: ",data)
-        let resp = await fetch("/h1",{
-            method: 'POST',
-            body: JSON.stringify(data),
-                headers:{
-                    'Content-Type': 'application/json'
-                }
-        });
-        let mensaje = await resp.json();
-        console.log(mensaje );
-        road=[];
-        if (poli){
-            map.removeLayer(poli)
-        }
-        if (mensaje.length ==0){
-            alert("Datos vacios, no hay nada que mostrar")
-        } else {
-            mensaje.map((d,i)=>{               
-                road[i]={
-                    lat: d.latitud,
-                    lon: d.longitud,
-                } 
-                roadtime[i]=d.fechaYhora
-            });
+	const date = new Date();
+	var mes = (date.getMonth() + 1) * 0.01;
+	def.value =
+		date.getFullYear() +
+		'sdferfsad' +
+		mes.toString().slice(2, 5) +
+		'-' +
+		date.getDate();
+	$(function () {
+		$('input[name="datetimes"]').daterangepicker(
+			{
+				timePicker: true,
+				timerFormat: 'HH:mm:ss',
+				singleDatePicker: false,
+				startDate: moment().subtract(12, 'hour'),
+				endDate: moment(),
+				timePicker24Hour: false,
+				timePickerIncrement: 15,
+				timePickerSeconds: false,
+				maxDate: moment(),
+				startValue: moment(),
+				locale: {
+					format: 'YYYY-MM-DD HH:mm:ss A',
+				},
+			},
+			function (start, end) {}
+		);
+		$('#dtp').html(
+			moment().subtract('days', 29).format('D MMMM YYYY') +
+				' - ' +
+				moment().format('D MMMM YYYY')
+		);
+	});
 
+	btn.addEventListener('click', async function () {
+		f1 = def.value.slice(0, 10);
+		f2 = def.value.slice(24, 35);
+		h1 = def.value.slice(11, 19);
+		h2 = def.value.slice(36, 44);
+		// console.log(def.value)
+		data = {
+			fecha1: f1,
+			fecha2: f2,
+			hora1: h1,
+			hora2: h2,
+		};
 
-            poli = L.polyline(road).addTo(map);
-            let range = document.getElementById("range")
-            let Aparecer=document.getElementById("Aparecer")
-            range.min = 0
-            range.max= road.length -1
-            range.oninput = ()=>{
-                marcador.setLatLng(road[range.value]).addTo(map)
-                document.getElementById("Texto").innerHTML= roadtime[range.value]
-            }
-            range.style.display="block"
-            Aparecer.style.display="inline"
-            
-        }
-            
+		// console.log("La consulta es: ",data)
+		var popup = document.getElementById('popup-historicos');
+		var opcion = popup.value;
+		if (opcion == 1) {
+			let resp = await fetch('/h11', {
+				method: 'POST',
+				body: JSON.stringify(data),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			mensaje = await resp.json();
+			console.log(mensaje);
+			road = [];
+			map.removeLayer(marcador);
+			marcador = L.marker([0, 0]);
+			marcador.addTo(map);
+			if (poli) {
+				map.removeLayer(poli);
+			}
+			if (mensaje.length == 0) {
+				alert('Datos vacios, no hay nada que mostrar');
+			} else {
+				mensaje.map((d, i) => {
+					road[i] = {
+						lat: d.latitud,
+						lon: d.longitud,
+					};
+					roadtime[i] = d.fechaYhora;
+				});
 
+				poli = L.polyline(road).addTo(map);
 
-    });
+				let range = document.getElementById('range');
+				range.min = 0;
+				range.max = road.length - 1;
+				range.oninput = () => {
+					marcador.setLatLng(road[range.value]).addTo(map);
+				};
+			}
+			range.style.display = 'block';
+		} else if (opcion == 2) {
+			let resp = await fetch('/h12', {
+				method: 'POST',
+				body: JSON.stringify(data),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+			mensaje = await resp.json();
+			console.log(mensaje);
+			map.removeLayer(marcador);
+			road = [];
+			marcador = L.marker([0, 0], { icon: greenIcon });
+			marcador.addTo(map);
+			if (poli) {
+				map.removeLayer(poli);
+			}
+			if (mensaje.length == 0) {
+				alert('Datos vacios, no hay nada que mostrar');
+			} else {
+				mensaje.map((d, i) => {
+					road[i] = {
+						lat: d.latitud,
+						lon: d.longitud,
+					};
+					roadtime[i] = d.fechaYhora;
+				});
 
-    
+				poli = L.polyline(road).addTo(map);
+				poli.setStyle({ color: 'red' });
 
-}
+				let range = document.getElementById('range');
+				range.min = 0;
+				range.max = road.length - 1;
+				range.oninput = () => {
+					marcador.setLatLng(road[range.value]).addTo(map);
+				};
+			}
+			range.style.display = 'block';
+		}
+	});
+};
--- public/js/index.js
+++ public/js/index.js
@@ -4,50 +4,107 @@ let latitud;
 let longitud;
 let fecha_hora;
 var marker = null;
-let map = L.map('map').setView([10.987785, -74.805640], 15);
+var marker2 = null;
+var greenIcon = new L.Icon({
+	iconUrl:
+		'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+	shadowUrl:
+		'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	iconSize: [25, 41],
+	iconAnchor: [12, 41],
+	popupAnchor: [1, -34],
+	shadowSize: [41, 41],
+});
+let map = L.map('map').setView([10.987785, -74.80564], 15);
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-    maxZoom: 18,
+	attribution:
+		'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	maxZoom: 18,
 }).addTo(map);
-let poly 
+let poly;
+let poly2;
 L.control.scale().addTo(map);
-function update() {
-    fetch('/data',{headers:{
-        'Content-Type': 'application/json'
-    }})
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(json);
-            
-            latitud = json['latitud'];
-            longitud = json['longitud'];
-            fecha_hora = json['fechaYhora'];
-            let NewLatLng = new L.LatLng(latitud,longitud)
-            if(!poly){
-                poly = L.polyline([{lat:latitud,lon:longitud}]).addTo(map)
-            }else{
-                poly.addLatLng(NewLatLng)
-            }
-            map.setView(NewLatLng)
-            document.getElementById(
-                'showLatitude'
-            ).innerHTML = `Latitud : ${latitud}`;
-            document.getElementById(
-                'showLongitude'
-            ).innerHTML = `Longitud : ${longitud}`;
-            document.getElementById(
-                'showFecha'
-            ).innerHTML = `Fecha y hora : ${fecha_hora}`;
-            if (marker !== null) {
-                map.removeLayer(marker);
-            }
-            marker = L.marker([latitud, longitud]).addTo(map);
-        });
+
+async function update1() {
+	fetch('/data1', {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => {
+			console.log(json);
+
+			latitud = json['latitud'];
+			longitud = json['longitud'];
+			fecha_hora = json['fechaYhora'];
+
+			let NewLatLng = new L.LatLng(latitud, longitud);
+			if (!poly) {
+				poly = L.polyline([{ lat: latitud, lon: longitud }]).addTo(map);
+			} else {
+				poly.addLatLng(NewLatLng);
+			}
+
+			document.getElementById(
+				'showLatitude'
+			).innerHTML = `Latitud : ${latitud}`;
+			document.getElementById(
+				'showLongitude'
+			).innerHTML = `Longitud : ${longitud}`;
+			document.getElementById(
+				'showFecha'
+			).innerHTML = `Fecha y hora : ${fecha_hora}`;
+			if (marker !== null) {
+				map.removeLayer(marker);
+			}
+			marker = L.marker([latitud, longitud]).addTo(map);
+		});
+}
+async function update2() {
+	fetch('/data2', {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => {
+			console.log(json);
+
+			latitud = json['latitud'];
+			longitud = json['longitud'];
+			fecha_hora = json['fechaYhora'];
+			id_camion = json['id_camion'];
+
+			let NewLatLng2 = new L.LatLng(latitud, longitud);
+
+			if (!poly2) {
+				poly2 = L.polyline([{ lat: latitud, lon: longitud }]).addTo(map);
+			} else {
+				poly2.addLatLng(NewLatLng2);
+			}
+
+			poly2.setStyle({ color: 'red' });
+
+			document.getElementById(
+				'showLatitude2'
+			).innerHTML = `Latitud : ${latitud}`;
+			document.getElementById(
+				'showLongitude2'
+			).innerHTML = `Longitud : ${longitud}`;
+			document.getElementById(
+				'showFecha2'
+			).innerHTML = `Fecha y hora : ${fecha_hora}`;
+			if (marker2 !== null) {
+				map.removeLayer(marker2);
+			}
+			marker2 = L.marker([latitud, longitud], { icon: greenIcon }).addTo(map);
+		});
 }
 
-setInterval(update,1000)
-
-
+setInterval(function () {
+	update1();
+	update2();
+}, 1000);
 
 // map.on('refresh', update);
